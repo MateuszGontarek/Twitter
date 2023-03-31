958cacf2cae0d8ea37b02efd73508a181ea684e0
nobodynek2019 <benedictus.gnatus@gmail.com>
2023-03-31 16:31:08 +0200

    Setup Commit

--- server/models/twit.js
+++ server/models/twit.js
@@ -17,6 +17,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: null,
   },
+  hastags: {
+    type: Array,
+    default: [],
+    required: false,
+  }
 });
 
 const Twit = mongoose.model("twits", userSchema);
